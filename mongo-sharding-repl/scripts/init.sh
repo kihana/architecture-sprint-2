7bd4ee2bed3786e401cfdaa52a9250be71daee45
kihana <ea.zayac@gmail.com>
2025-01-14 23:07:58 +0100

    Added number of replications for each shards.

---
+++ mongo-sharding-repl/scripts/init.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Инициализация конфигурационного MongoDB сервера
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+exit();
+EOF
+
+#Инициализация MongoDB шардов
+docker compose exec -T shard1-primary mongosh --port 27018 <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1-primary:27018" },
+        { _id : 1, host : "shard1-secondary1:27021" },
+        { _id : 2, host : "shard1-secondary2:27022" }
+      ]
+    }
+);
+rs.status();
+exit();
+EOF
+
+docker compose exec -T shard2-primary mongosh --port 27019 <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 0, host : "shard2-primary:27019" },
+        { _id : 1, host : "shard2-secondary1:27023" },
+        { _id : 2, host : "shard2-secondary2:27024" }
+      ]
+    }
+  );
+rs.status();
+exit();
+EOF
+
+# Инициaлизация MongoDB роутера и наполнение БД данными
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+sh.addShard( "shard1/shard1-primary:27018,shard1-secondary1:27021,shard1-secondary2:27022");
+sh.addShard( "shard2/shard2-primary:27019,shard2-secondary1:27023,shard2-secondary2:27024");
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
+use somedb;
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i});
+exit(); 
+EOF
